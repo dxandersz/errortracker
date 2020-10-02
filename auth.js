5782f24fb2ab77774c1d37851fec203460483298
David Anderson <d.c.anderson@live.com>
2020-10-02 03:26:25 -0400

    Login and signup working!

--- auth.js
+++ auth.js
@@ -25,21 +25,23 @@ router.get(
 );
 
 router.get("/callback", (req, res, next) => {
+ passport.authenticate("auth0", (err, user, info) => {
     if (err) {
         return next(err);
     }
-    if (!user) {
+    if(!user) {
         return res.redirect("/login");
     }
     req.logIn(user, (err) => {
-        if (err) {
+        if(err) {
             return next(err);
         }
         const returnTo = req.session.returnTo;
         delete req.session.returnTo;
         res.redirect(returnTo || "/");
     });
-})
+})(req, res, next);
+});
 
 router.get("/logout", (req, res) => {
     req/logOut();
--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
+      process.env.AUTH0_CALLBACK_URL || "http://localhost:4000/callback"
   },
   function(accessToken, refreshToken, extraParams, profile, done) {
     return done(null, profile);
