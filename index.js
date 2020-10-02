f21177b2ef95f0d260f88dd5a0466d83e8147b6a
David Anderson <d.c.anderson@live.com>
2020-10-02 17:31:17 -0400

    Reverted back, trading in Auth0 for bcrypt.

--- index.js
+++ index.js
@@ -4,58 +4,25 @@
  * Required External Modules
  */
 
-const express = require("express");
+const express =require("express");
 const path = require("path");
-
-const expressSession = require("express-session");
-const passport = require("passport");
-const Auth0Strategy = require("passport-auth0");
-
-require("dotenv").config();
-
-const authRouter = require("./auth");
 
 /**
   * App Variables
   */
 
-
-
 const app = express();
-const port = process.env.port || "4000";
+const port = process.env.PORT || "4000";
 
 /**
   * Session Configuration
   */
 
-const session = {
-    secret: "Onetimeadonkeyateacarrot!",
-    cookie: {},
-    resave: false,
-    saveUninitialized: false
-};
-
-if (app.get("env") === "production") {
-    // Serve secure cookies, needs HTTPS
-    session.cookie.secure = true;
-}
 
 /**
   * Passport Configuration
   */
 
-const strategy = new Auth0Strategy(
-  {
-    domain: process.env.AUTH0_DOMAIN,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:
-      process.env.AUTH0_CALLBACK_URL || "http://localhost:4000/callback"
-  },
-  function(accessToken, refreshToken, extraParams, profile, done) {
-    return done(null, profile);
-  }
-);
 
 /**
    * App Configuration
@@ -63,30 +30,13 @@ const strategy = new Auth0Strategy(
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(expressSession(session));
-
-passport.use(strategy);
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
-
-app.use("/", authRouter);
 
 /**
     * Routes Definitions
     */
 
 app.get("/", (req, res) => {
-    res.render("index", { title: "Home" });
+  res.render("index",{title: "Home" });
 });
 
 /** 
@@ -94,5 +44,5 @@ app.get("/", (req, res) => {
 */
 
 app.listen(port, () => {
-    console.log(`Listening to requests on https://localhost:${port}`);
-})
+  console.log(`Listening to requests on https://localhost:${port}`);
+});
